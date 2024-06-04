f5b3dbff48c1378d2aff36116efb3777f3aef64b
Rivest Osz <rivest-oss@proton.me>
2024-06-04 03:14:41 -0300

    Added "count" decoding

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 class ComprezzEncoder {
 	constructor() {};
 	
-	_buildCount(buff, reject) {
+	_buildCount(buff) {
 		let dict = [];
 		for(let i = 0; i < 0x100; i++)
 			dict.push(i);
@@ -29,12 +29,40 @@ class ComprezzEncoder {
 			if(Buffer.isBuffer(data) !== true)
 				return reject("`encode` expected a Buffer");
 			
-			const mtf = this._buildCount(data);
+			const count = this._buildCount(data);
+			
+			// [TODO]
 		});
 	};
 };
 
 class ComprezzDecoder {
+	constructor() {};
+	
+	_buildInvCount(buff, reject) {
+		let dict = [];
+		for(let i = 0; i < 0x100; i++)
+			dict.push(i);
+		
+		const ret = Buffer.alloc(buff.length);
+		
+		for(let i = 0, c = 0x00, j = 0; i < buff.length; i++) {
+			j = buff.readUInt8(i);
+			
+			c = dict[j];
+			ret[i] = c;
+			
+			dict.splice(j, 1);
+			dict.unshift(c);
+		};
+		
+		return ret;
+	};
+	
+	// `decode` expects a Buffer, and maybe a number.
+	decode(data, sizeLimit = false) {
+		// [TODO]
+	};
 };
 
 module.exports = { Encoder: ComprezzEncoder, Decoder: ComprezzDecoder };
