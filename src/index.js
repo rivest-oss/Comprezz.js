2b2ed24a7b95ea437efe031e270a83c7556a4b1c
Rivest Osz <rivest-oss@proton.me>
2024-06-04 02:55:00 -0300

    Added basic "count" algorithm

---
+++ src/index.js
@@ -0,0 +1,40 @@
+"use strict";
+
+class ComprezzEncoder {
+	constructor() {};
+	
+	_buildCount(buff, reject) {
+		let dict = [];
+		for(let i = 0; i < 0x100; i++)
+			dict.push(i);
+		
+		const ret = Buffer.alloc(buff.length);
+		
+		for(let i = 0, c = 0x00, j = 0; i < buff.length; i++) {
+			c = buff.readUInt8(i);
+			j = dict.indexOf(c);
+			
+			ret[i] = j;
+			
+			dict.splice(j, 1);
+			dict.unshift(c);
+		};
+		
+		return ret;
+	};
+	
+	// `encode` expects a Buffer.
+	encode(data) {
+		return new Promise((resolve, reject) => {
+			if(Buffer.isBuffer(data) !== true)
+				return reject("`encode` expected a Buffer");
+			
+			const mtf = this._buildCount(data);
+		});
+	};
+};
+
+class ComprezzDecoder {
+};
+
+module.exports = { Encoder: ComprezzEncoder, Decoder: ComprezzDecoder };
